ebe03b9d2739c96bc00574eb8c5f8bcf85f86195
Nishad Deshmukh <30819604+Nishaddeshmukh@users.noreply.github.com>
2024-04-25 00:23:39 +0530

    Create N-th_Tribonacci_Number.rs
    The Tribonacci sequence Tn is defined as follows: 
    
    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
    
    Given n, return the value of Tn.
    
     
    
    Example 1:
    
    Input: n = 4
    Output: 4
    Explanation:
    T_3 = 0 + 1 + 1 = 2
    T_4 = 1 + 1 + 2 = 4
    Example 2:
    
    Input: n = 25
    Output: 1389537
     
    
    Constraints:
    
    0 <= n <= 37
    The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.

---
+++ N-th_Tribonacci_Number.rs
@@ -0,0 +1,22 @@
+/*The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+*/
+
+impl Solution {
+    pub fn tribonacci(n: i32) -> i32 {
+        let mut tri = [0, 1, 1];
+        if n < 3 {
+            return tri[n as usize];
+        }
+        for _ in 3..=n {
+            let temp = tri[0] + tri[1] + tri[2];
+            tri[0] = tri[1];
+            tri[1] = tri[2];
+            tri[2] = temp;
+        }
+        tri[2]       
+    }
+}

7e541b9111cd779fbf8298540cf76dc73fdae64d
Nishad Deshmukh <30819604+Nishaddeshmukh@users.noreply.github.com>
2024-04-27 15:34:32 +0530

    Update N-th_Tribonacci_Number.rs
    commiting again to check changes relection

--- N-th_Tribonacci_Number.rs
+++ N-th_Tribonacci_Number.rs
@@ -5,6 +5,8 @@ T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 Given n, return the value of Tn.
 */
 
+
+
 impl Solution {
     pub fn tribonacci(n: i32) -> i32 {
         let mut tri = [0, 1, 1];
